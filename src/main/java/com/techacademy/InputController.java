9653eb6c669f6b30737678928be6e2e8f84ef0b4
yukogithub <netteoisii@gmail.com>
2023-02-28 17:44:57 +0900

    Lesson15課題再提出

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -1,10 +1,7 @@
 package com.techacademy;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InputController {
@@ -13,13 +10,5 @@ public class InputController {
     public String getInput() {
         //input.htmlに画面遷移
         return "input";
-    }
-
-    @PostMapping("/confirm")
-    public String postOutput(@RequestParam("val") String val, Model model) {
-        //フォームから送信されてきた値をModelに登録
-        model.addAttribute("val",val);
-        //output.htmlに画面遷移
-        return "output";
     }
 }
